b307c409c1e9cbf74cddf5d1e06a2252485df034
emilymeng <eeng@seattletimes.com>
2021-02-18 16:18:30 -0800

    new jw player swap

--- src/js/bc-preroll.js
+++ src/js/bc-preroll.js
@@ -2,8 +2,8 @@ var $ = require("./lib/qsa");
 
 var makePlayer = function(id) {
   return `
-<iframe src="//players.brightcove.net/1509317113/V1eCvUwO2g_default/index.html?videoId=${id}&autoplay=true" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
-`;
+  <iframe src="//cdn.jwplayer.com/players/${id}-oNGue23q.html" width="100%" height="100%" frameborder="0" scrolling="auto" style="position:absolute;" allowfullscreen></iframe>
+  `;
 };
 
 $(".bc-preroll").forEach(function(vidbox) {
